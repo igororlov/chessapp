a4fb506e9eca69bd1fa1277cc3a5e7e13d39d23b
Igor Orlov <igororlov92@mail.ru>
2014-01-16 20:38:47 +0100

    Created chessapp (from vitapp).

---
+++ src/main/java/ua/chessapp/web/BaseController.java
@@ -0,0 +1,26 @@
+package ua.chessapp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Controller
+@RequestMapping("/")
+public class BaseController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BaseController.class);
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String mainPage() {
+		logger.info("Inside main page");
+		return "index"; 
+	}
+
+	@RequestMapping(value="/welcome", method = RequestMethod.GET)
+	public String welcome() {
+		logger.info("Inside welcome");
+		return "welcome"; 
+	}
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,7 +1,10 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<!DOCTYPE html>
+
 <html>
 <body>
 <h2>Hello World!</h2>
- 
-<h4>Message : ${message}</h1>	
+ 	
 </body>
 </html>
